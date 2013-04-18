71ea941faacdc1e835336288996384309684165a
Greg Mercer <gmercer@stanford.edu>
2013-04-18 13:24:52 -0700

    Create after_build.php

---
+++ after_build.php
@@ -0,0 +1,18 @@
+<?php
+
+$ini_array = parse_ini_file("../settings/config.ini");
+
+if (isset($ini_array['acquia_key'])) {
+  variable_set('acquia_key', $ini_array['acquia_key']);
+}
+
+if (isset($ini_array['acquia_identifier'])) {
+  variable_set('acquia_identifier', $ini_array['acquia_identifier']);
+}
+
+if (isset($ini_array['acquia_subscription_name'])) {
+  variable_set('acquia_subscription_name', $ini_array['acquia_subscription_name']);
+}
+
+acquia_agent_check_subscription();
+

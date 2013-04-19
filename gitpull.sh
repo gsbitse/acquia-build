a1b28eb013683591a405aa4f03dda94156a4171c
Greg Mercer <gmercer@stanford.edu>
2013-04-18 20:21:56 -0700

    Create gitpull.sh

---
+++ gitpull.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#####################################################
+# run git pull to update the files in acquia-build
+#
+
+echo "running git pull to update acquia-build"
+git pull
+echo "end of git pull to update acquia-build"

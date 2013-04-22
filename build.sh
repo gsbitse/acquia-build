5a541b4f7bef2350fd85e0dcccfc1f9614123823
Greg Mercer <gmercer@stanford.edu>
2013-04-22 09:45:47 -0700

    Adding in support for the apc clear cache step

--- build.sh
+++ build.sh
@@ -22,41 +22,47 @@ echo "account email: $ACCTEMAIL"
 #
 
 build_dir="/mnt/gfs/home/gsbpublic/build/bin/acquia-build"
-site_url="http://gsbpublicdev2.prod.acquia-sites.com/"
 
 if test $1 = "dev"
 then
   cd /mnt/www/html/gsbpublicdev/docroot
+  site_url="http://gsbpublicdev.prod.acquia-sites.com/"
 fi
 
 if test $1 = "dev2"
 then
   cd /mnt/www/html/gsbpublicdev2/docroot
+  site_url="http://gsbpublicdev.prod.acquia-sites.com/"
 fi
 
 if test $1 = "stage"
 then
   cd /mnt/www/html/gsbpublicstg/docroot
+  site_url="http://gsbpublicstg.prod.acquia-sites.com/"
 fi
 
 if test $1 = "stage2"
 then
   cd /mnt/www/html/gsbpublicstg2/docroot
+  site_url="http://gsbpublicstg2.prod.acquia-sites.com/"
 fi
 
 if test $1 = "sandbox"
 then
   cd /mnt/www/html/gsbpublicsand/docroot
+  site_url="http://gsbpublicsand.prod.acquia-sites.com/"
 fi
 
 if test $1 = "loadtest"
 then
   cd /mnt/www/html/gsbpubliclt/docroot
+  site_url="http://gsbpubliclt.prod.acquia-sites.com/"
 fi
 
 if test $1 = "prod"
 then
   cd /mnt/www/html/gsbpublic/docroot
+  site_url="http://gsbpublic.prod.acquia-sites.com/"
 fi
 
 pwd

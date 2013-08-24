5807dafc48759ea61a523cecfa5ec490f8e1fd3e
Greg Mercer <gmercer@stanford.edu>
2013-08-24 03:14:26 -0700

    Changes to support prod only build

--- build_prod.sh
+++ build_prod.sh
@@ -4,7 +4,7 @@
 # run update.php on the site
 #
 
-echo "start of build.sh on acquia: server = $1 rebuild = $2"
+echo "start of build_prod.sh on acquia: server = $1 rebuild = $2"
 
 ##########################################################
 # get the password and account email
@@ -23,41 +23,8 @@ echo "account email: $ACCTEMAIL"
 
 build_dir="/mnt/gfs/home/gsbpublic/build/bin/acquia-build"
 
-if test $1 = "dev"
-then
-  cd /mnt/www/html/gsbpublicdev/docroot
-  site_url="http://gsbpublicdev.prod.acquia-sites.com/"
-fi
-
-if test $1 = "dev2"
-then
-  cd /mnt/www/html/gsbpublicdev2/docroot
-  site_url="http://gsbpublicdev2.prod.acquia-sites.com/"
-fi
-
-if test $1 = "stage"
-then
-  cd /mnt/www/html/gsbpublicstg/docroot
-  site_url="http://gsbpublicstg.prod.acquia-sites.com/"
-fi
-
-if test $1 = "stage2"
-then
-  cd /mnt/www/html/gsbpublicstg2/docroot
-  site_url="http://gsbpublicstg2.prod.acquia-sites.com/"
-fi
-
-if test $1 = "sandbox"
-then
-  cd /mnt/www/html/gsbpublicsand/docroot
-  site_url="http://gsbpublicsand.prod.acquia-sites.com/"
-fi
-
-if test $1 = "loadtest"
-then
-  cd /mnt/www/html/gsbpubliclt/docroot
-  site_url="http://gsbpubliclt.prod.acquia-sites.com/"
-fi
+cd /mnt/www/html/gsbpublic/docroot
+site_url="http://gsbpublic.prod.acquia-sites.com/"
 
 pwd
 
@@ -73,41 +40,25 @@ echo "Sleeping for 2 minutes to give time for the code to appear."
 sleep 120
 
 ##########################################################
-# run drush site install and script (after the install) 
+# run update.php
 #
 
-ret_code=0
-
-if test $2 = "true"
-then
-  ret_code=$(drush si -y --site-name="gsbpublic" --account-pass="$PASSWORD" --acount-mail="$ACCTEMAIL" gsb_public)
-  echo "drush si ret_code = $ret_code"
-fi
-
-##########################################################
-# save the docroot directory
-#
-
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush updb -y)
-  echo "drush updb ret_code = $ret_code"
-fi
+#cd ${docroot_dir}
+#ret_code=$(drush updb -y)
+#echo "drush updb ret_code = $ret_code"
 
 ##########################################################
 # do feature revert
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush eval "features_revert();")
-  echo "drush features_revert ret_code = $ret_code"
-fi
+#if test $2 != "true"
+#then
+#  cd ${docroot_dir}
+#  ret_code=$(drush eval "features_revert();")
+#  echo "drush features_revert ret_code = $ret_code"
+#fi
 
-
-echo "end of build.sh on acquia"
+echo "end of build_prod.sh on acquia"
 
 return 0
 

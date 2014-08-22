2a47b659b9ba3ebda0850ae31007a981d1b2fe84
Adam Moore <redndahead@gmail.com>
2014-08-21 17:03:16 -0700

    Remove support for fresh install.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # run update.php on the site
 #
 
-echo "start of build.sh on acquia: server = $1 rebuild = $2"
+echo "start of build.sh on acquia: server = $1"
 
 ##########################################################
 # get the password and account email
@@ -27,16 +27,6 @@ rm update-finished.txt
 
 cd ~/build
 
-
-### OLD ###
-#read -r PASSWORD < settings/password.txt
-#read -r ACCTEMAIL < settings/email.txt
-
-PASSWORD=blah
-ACCTEMAIL=blah@example.com
-
-echo "account email: $ACCTEMAIL"
-
 ##########################################################
 # change to the docroot directory
 #
@@ -46,25 +36,21 @@ build_dir="/mnt/gfs/home/gsbpublic/build/bin/acquia-build"
 if test $1 = "dev"
 then
   cd /mnt/www/html/gsbpublicdev/docroot
-  site_url="http://gsbpublicdev.prod.acquia-sites.com/"
 fi
 
 if test $1 = "dev2"
 then
   cd /mnt/www/html/gsbpublicdev2/docroot
-  site_url="http://gsbpublicdev2.prod.acquia-sites.com/"
 fi
 
 if test $1 = "stage"
 then
   cd /mnt/www/html/gsbpublicstg/docroot
-  site_url="http://gsbpublicstg.prod.acquia-sites.com/"
 fi
 
 if test $1 = "stage2"
 then
   cd /mnt/www/html/gsbpublicstg2/docroot
-  site_url="http://gsbpublicstg2.prod.acquia-sites.com/"
 fi
 
 pwd
@@ -76,89 +62,62 @@ pwd
 docroot_dir=$PWD
 
 ##########################################################
-# sleep for 2 minutes to give time for the pushed code to appear.
-#echo "Sleeping for 2 minutes to give time for the code to appear."
-#sleep 120
-
-##########################################################
 # run drush site install and script (after the install) 
 #
 
 ret_code=0
 
-if test $2 = "true"
-then
-  ret_code=$(drush5 si -y --site-name="gsbpublic" --account-pass="$PASSWORD" --account-mail="$ACCTEMAIL" gsb_public)
-  echo "drush5 si ret_code = $ret_code"
-fi
-
 ##########################################################
-# save the docroot directory
+# run updates
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush5 updb -y)
-  echo "drush5 updb ret_code = $ret_code"
-fi
+cd ${docroot_dir}
+ret_code=$(drush5 updb -y)
+echo "drush5 updb ret_code = $ret_code"
 
 ##########################################################
 # do feature revert
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush5 fra -y)
-  echo "drush5 features_revert ret_code = $ret_code"
-fi
+cd ${docroot_dir}
+ret_code=$(drush5 fra -y)
+echo "drush5 features_revert ret_code = $ret_code"
+
 
 ##########################################################
 # Set our google analytics account for stage.
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush5 vset -y 'googleanalytics_account' 'UA-17436788-1')
-  echo "drush5 vset -y 'googleanalytics_account' 'UA-17436788-1' = $ret_code"
-fi
+cd ${docroot_dir}
+ret_code=$(drush5 vset -y 'googleanalytics_account' 'UA-17436788-1')
+echo "drush5 vset -y 'googleanalytics_account' 'UA-17436788-1' = $ret_code"
+
 
 ##########################################################
 # Set the faculty profile webservice url.
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush5 vset -y 'gsb_feature_faculty_ct_service_url' 'https://gsbapps-qa.stanford.edu/utilitywebservice/global/UtilityWebservice.asmx')
-  echo "drush5 vset -y 'gsb_feature_faculty_ct_service_url' 'https://gsbapps-qa.stanford.edu/utilitywebservice/global/UtilityWebservice.asmx' = $ret_code"
-fi
+cd ${docroot_dir}
+ret_code=$(drush5 vset -y 'gsb_feature_faculty_ct_service_url' 'https://gsbapps-qa.stanford.edu/utilitywebservice/global/UtilityWebservice.asmx')
+echo "drush5 vset -y 'gsb_feature_faculty_ct_service_url' 'https://gsbapps-qa.stanford.edu/utilitywebservice/global/UtilityWebservice.asmx' = $ret_code"
 
 ##########################################################
 # Enable development modules
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush5 en -y devel)
-  echo "drush5 en -y devel = $ret_code"
-  ret_code=$(drush5 vd)
-  echo "drush5 vd = $ret_code"
-fi
+cd ${docroot_dir}
+ret_code=$(drush5 en -y devel)
+echo "drush5 en -y devel = $ret_code"
+ret_code=$(drush5 vd)
+echo "drush5 vd = $ret_code"
 
 ##########################################################
 # do a clear cache all
 #
 
-if test $2 != "true"
-then
-  cd ${docroot_dir}
-  ret_code=$(drush5 cc all)
-  echo "drush5 cc all ret_code = $ret_code"
-fi
+cd ${docroot_dir}
+ret_code=$(drush5 cc all)
+echo "drush5 cc all ret_code = $ret_code"
 
 
 echo "end of build.sh on acquia"
@@ -168,4 +127,3 @@ return 0
 ##########################################################
 # end of build script
 #
-
